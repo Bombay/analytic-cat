483146cea4f12fc9717fbc1a958c78603d383cf3
siam <ojcat0419@gmail.com>
2023-11-04 14:13:48 +0900

    radix-theme 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import { radixThemePreset } from 'radix-themes-tw'
 
 const config: Config = {
   content: [
@@ -6,15 +7,10 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  presets: [radixThemePreset],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require('postcss-import')],
 }
 export default config
