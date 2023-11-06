28d9763e0799de5689a1d1fec9bea9732f4ba776
hyuntkim <hyuntkim@smilegate.com>
2023-11-06 16:37:37 +0900

    shadcn-ui test components 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Label } from '@/components/ui/label'
+
 export default function Home() {
-  return <div>home</div>
+  return (
+    <div>
+      <Label htmlFor="terms">Accept terms and conditions</Label>
+    </div>
+  )
 }
